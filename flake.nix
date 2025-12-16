bd68ef3b805612e83a190092e9aa1dbfb94dd366
Ben Currie <77416358+DonutL0rd@users.noreply.github.com>
2025-12-16 11:20:39 -0800

    added flake nixos support

--- flake.nix
+++ flake.nix
@@ -1,28 +1,39 @@
 {
-    description = "Home Manager";
-
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-        home-manager = {
-            url = "github:nix-community/home-manager";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+  description = "Home Manager and NixOS";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    outputs = {self, nixpkgs, home-manager, ...}: {
-        homeConfigurations = {
-            "benjamincurrie" = home-manager.lib.homeManagerConfiguration {
-                # System is very important!
-                pkgs = import nixpkgs { system = "aarch64-darwin"; };
-
-                modules = [ ./shell.nix ./profiles/benjamincurrie.nix]; # Defined later
-            };
-            "nrm" = home-manager.lib.homeManagerConfiguration {
-                # System is very important!
-                pkgs = import nixpkgs { system = "x86_64-linux"; };
-
-                modules = [ ./shell.nix ./profiles/nrm.nix]; # Defined later
-            };
-        };
+  };
+  
+  outputs = {self, nixpkgs, home-manager, ...}: {
+    # Your existing Home Manager configs
+    homeConfigurations = {
+      "benjamincurrie" = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs { system = "aarch64-darwin"; };
+        modules = [ ./shell.nix ./profiles/benjamincurrie.nix ];
+      };
+      "nrm" = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+        modules = [ ./shell.nix ./profiles/nrm.nix ];
+      };
     };
+    
+    # NEW: NixOS system configuration
+    nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";  # M4 Mac = ARM
+      modules = [
+        ./hosts/m4virt.nix
+        # Optional: integrate Home Manager into NixOS
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.benjamincurrie = import ./shell.nix;  # or create a new VM-specific profile
+        }
+      ];
+    };
+  };
 }
